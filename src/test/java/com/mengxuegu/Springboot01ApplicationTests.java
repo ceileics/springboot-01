77b14c7fd1bf1be1b6d8103163f4b3a1803be01f
zxh <307606523@qq.com>
2020-02-23 20:04:36 +0800

    第一次提交

---
+++ src/test/java/com/mengxuegu/Springboot01ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mengxuegu;
+
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
